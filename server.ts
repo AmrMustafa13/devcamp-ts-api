ee3f119d11288da3c4c20cf002c2aae01858e726
Amr Mustafa <amrmostafa1311@gmail.com>
2023-10-19 16:35:28 +0300

    added morgan logger

--- server.ts
+++ server.ts
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 
 import bootcamps from "./routes/bootcamps";
+import morgan from "morgan";
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use("/api/v1/bootcamps", bootcamps);
 
