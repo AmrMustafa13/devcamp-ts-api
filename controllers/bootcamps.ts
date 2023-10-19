443caa009cac48f54195b9d2248783874762a686
Amr Mustafa <amrmostafa1311@gmail.com>
2023-10-19 20:35:57 +0300

    implemented bootcamps controllers

--- controllers/bootcamps.ts
+++ controllers/bootcamps.ts
@@ -1,37 +1,78 @@
 import { RequestHandler } from "express";
+import Bootcamp from "../models/Bootcamp";
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 
 export const getBootcamps: RequestHandler = async (req, res, next) => {
-  res.send("Hello World!");
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error: any) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
 export const getBootcamp: RequestHandler = async (req, res, next) => {
-  res.send(`Hello World ${req.params.id}`);
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp)
+      return res
+        .status(400)
+        .json({ success: false, data: "No bootcamp found" });
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error: any) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
 export const createBootcamp: RequestHandler = async (req, res, next) => {
-  res.send("Hello World!");
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error: any) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 export const updateBootcamp: RequestHandler = async (req, res, next) => {
-  res.send(`Hello World ${req.params.id}`);
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!bootcamp)
+      return res
+        .status(400)
+        .json({ success: false, data: "No bootcamp found" });
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error: any) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 export const deleteBootcamp: RequestHandler = async (req, res, next) => {
-  res.send(`Hello World ${req.params.id}`);
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!bootcamp)
+      return res
+        .status(400)
+        .json({ success: false, data: "No bootcamp found" });
+    res.status(200).json({ success: true, data: {} });
+  } catch (error: any) {
+    res.status(400).json({ success: false });
+  }
 };
--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ import bootcamps from "./routes/bootcamps";
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+app.use(express.json());
 
 app.use(morgan("dev"));
 
