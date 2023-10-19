fdbaeee0354223b780786fa45f11bbd6fe92ac7a
Amr Mustafa <amrmostafa1311@gmail.com>
2023-10-19 21:19:36 +0300

    more error customization

--- middlewares/error.ts
+++ middlewares/error.ts
@@ -2,11 +2,21 @@ import { ErrorRequestHandler } from "express";
 import { ErrorResponse } from "../utils/errorResponse";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.log(err.stack);
+  console.log(err);
 
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     err = new ErrorResponse(message, 404);
+  }
+
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    err = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val: any) => val.message);
+    err = new ErrorResponse(message.toString(), 400);
   }
 
   res
